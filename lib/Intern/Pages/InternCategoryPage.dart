01d49a5634c24470a9e0aa1ecf3f080e3fb2f8f7
BisratHailu <bisrathailu793@yahoo.com>
2020-09-09 23:38:35 +0300

    Inter specific updates!! - provide search access for Intern - choose categories to check out for updates - get new updates on specif category

--- lib/Intern/Pages/InternCategoryPage.dart
+++ lib/Intern/Pages/InternCategoryPage.dart
@@ -11,6 +11,8 @@ class _InternCategoryPageState extends State<InternCategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+     
       appBar: AppBar(
         title: Text("Intern Platform"),
         elevation: 0.0,
@@ -22,107 +24,117 @@ class _InternCategoryPageState extends State<InternCategoryPage> {
         ],
       ),
       drawer: Drawer(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("My Profile"),
-            ),
-            ListTile(
-              leading: Icon(Icons.description),
-              title: Text("My Applications"),
-            )
-          ],
+        child: SafeArea(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("My Profile"),
+              ),
+              ListTile(
+                leading: Icon(Icons.description),
+                title: Text("My Applications"),
+              )
+            ],
+          ),
         ),
       ),
       body: Container(
         color: myColor.myBlack,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Hi Intern',
-                style: TextStyle(
-                    color: myColor.myWhite,
-                    fontFamily: 'Oswald',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25),
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                  color: myColor.myGrey,
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextFormField(
-                cursorColor: myColor.myBlack,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Text(
-                    "Categories",
-                    style: TextStyle(color: myColor.myWhite, fontSize: 18),
-                  ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Hi Intern',
+                  style: TextStyle(
+                      color: myColor.myWhite,
+                      fontFamily: 'Oswald',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
                 ),
-                Container(
-                    height: 75,
-                    child: ListView.builder(
-                      itemCount: 20,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          width: 75,
-                          margin: EdgeInsets.symmetric(horizontal: 10),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    color: myColor.myGrey,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextFormField(
+                  decoration: InputDecoration(
+
+                    fillColor: myColor.myBlack,
+                 hoverColor: Colors.transparent,
+                    icon: Icon(Icons.search,color: myColor.myBlack,)
+                  ),
+                  cursorColor: myColor.myBlack,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: Text(
+                      "Categories",
+                      style: TextStyle(color: myColor.myWhite, fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                      height: 75,
+                      child: ListView.builder(
+                        itemCount: 20,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            width: 75,
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Center(child: Text("Software")),
                             ),
-                            child: Center(child: Text("Software")),
-                          ),
-                        );
-                      },
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-           Expanded(child: Container(
-            width: MediaQuery.of(context).size.width,
-             decoration: BoxDecoration(
-                 color: myColor.myWhite,
-               borderRadius: BorderRadius.only(topRight: Radius.circular(25),topLeft: Radius.circular(25))
-             ),
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 SizedBox(height: 10,),
-                 Padding(
-                   padding:  EdgeInsets.only(left: 20,top: 10),
-                   child: Text("Your Choice",style: TextStyle(fontFamily: 'Oswald',fontSize: 20),),
-                 ),
-               ],
-             ),
-           ))
-          ],
+                          );
+                        },
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+             Expanded(child: Container(
+              width: MediaQuery.of(context).size.width,
+               decoration: BoxDecoration(
+                   color: myColor.myWhite,
+                 borderRadius: BorderRadius.only(topRight: Radius.circular(25),topLeft: Radius.circular(25))
+               ),
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   SizedBox(height: 10,),
+                   Padding(
+                     padding:  EdgeInsets.only(left: 20,top: 10),
+                     child: Text("Your Choice",style: TextStyle(fontFamily: 'Oswald',fontSize: 20),),
+                   ),
+                 ],
+               ),
+             ))
+            ],
+          ),
         ),
-      ),
+
     );
   }
 }
--- lib/Utilities/variables.dart
+++ lib/Utilities/variables.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class myColor{
-  static final  Color myBlack = Color(0xff485063);
+  static final  Color myBlack = Color(0xff070810);
   static final  Color myWhite = Colors.white;
   static final  Color myGrey = Color(0xff9398B0);
 
