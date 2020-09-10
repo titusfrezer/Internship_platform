bb3267a8c820b9e3670e2d535306058cd0ce1e83
BisratHailu <bisrathailu793@yahoo.com>
2020-09-10 14:11:35 +0300

    Inter specific updates!! - provide search access for Intern - choose categories to check out for updates - get new updates on specif category - some updated ui features

--- lib/Utilities/variables.dart
+++ lib/Utilities/variables.dart
@@ -4,5 +4,6 @@ class myColor{
   static final  Color myBlack = Color(0xff070810);
   static final  Color myWhite = Colors.white;
   static final  Color myGrey = Color(0xff9398B0);
+  static final  Color myPurple = Color(0xff745481);
 
 }
