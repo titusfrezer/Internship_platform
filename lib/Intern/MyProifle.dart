9288b31ccf1718f8b57578cebb51edf38f9d0529
BisratHailu <bisrathailu793@yahoo.com>
2020-12-22 18:13:20 +0300

    Revert "New intern UI update"
    Token removed

--- lib/Intern/MyProifle.dart
+++ lib/Intern/MyProifle.dart
@@ -64,6 +64,11 @@ class _MyProfileState extends State<MyProfile> {
         .equalTo(widget.email);
     return Scaffold(
         resizeToAvoidBottomPadding: true,
+        backgroundColor: myColor.myBackground,
+        appBar: AppBar(
+          title: Text("My Profile"),
+          backgroundColor: myColor.myBackground,
+        ),
         body: Form(
           key: _formKey,
           autovalidateMode: AutovalidateMode.always,
@@ -81,6 +86,7 @@ class _MyProfileState extends State<MyProfile> {
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      SizedBox(height: 10,),
                       GestureDetector(
                         onTap: () async {
                           print('hi');
@@ -121,26 +127,7 @@ class _MyProfileState extends State<MyProfile> {
                                         ),
                                       ),
                                     ),
-                                    Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            horizontal: 15, vertical: 10),
-                                        child: Container(
-                                          height: 50,
-                                          width: 50,
-                                          decoration: BoxDecoration(
-                                              color: Colors.grey.shade400,
-                                              borderRadius:
-                                                  BorderRadius.circular(20)),
-                                          child: IconButton(
-                                              icon: Icon(Icons.arrow_back),
-                                              onPressed: () {
-                                                Navigator.of(context).pop();
-                                              }),
-                                        ),
-                                      ),
-                                    ),
+
                                   ],
                                 ),
                               )
@@ -181,27 +168,7 @@ class _MyProfileState extends State<MyProfile> {
                                             ),
                                           ),
                                         ),
-                                        Align(
-                                          alignment: Alignment.topLeft,
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 15, vertical: 20),
-                                            child: Container(
-                                              height: 50,
-                                              width: 50,
-                                              decoration: BoxDecoration(
-                                                  color: Colors.grey.shade400,
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          20)),
-                                              child: IconButton(
-                                                  icon: Icon(Icons.arrow_back),
-                                                  onPressed: () {
-                                                    Navigator.of(context).pop();
-                                                  }),
-                                            ),
-                                          ),
-                                        ),
+
                                       ],
                                     ),
                                   )
@@ -212,56 +179,13 @@ class _MyProfileState extends State<MyProfile> {
                                       mainAxisAlignment:
                                           MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Align(
-                                          alignment: Alignment.topCenter,
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 15, vertical: 10),
-                                            child: Row(
-                                              children: [
-                                                Container(
-                                                  height: 50,
-                                                  width: 50,
-                                                  decoration: BoxDecoration(
-                                                      color:
-                                                          Colors.grey.shade400,
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              20)),
-                                                  child: IconButton(
-                                                      icon: Icon(
-                                                          Icons.arrow_back),
-                                                      onPressed: () {
-                                                        Navigator.of(context)
-                                                            .pop();
-                                                      }),
-                                                ),
-                                                Container(
-                                                  margin: EdgeInsets.only(
-                                                      left:
-                                                          MediaQuery.of(context)
-                                                                  .size
-                                                                  .width *
-                                                              0.15),
-                                                  alignment: Alignment.center,
-                                                  child: Text(
-                                                    "My Profile",
-                                                    style: TextStyle(
-                                                        fontSize: 18,
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
+
                                         Align(
                                           alignment: Alignment.center,
                                           child: CircleAvatar(
                                             backgroundColor: myColor.myWhite,
                                             child: Icon(Icons.person,
-                                                size: 100, color: Colors.black),
+                                                size: 100, color:myColor.myWhite),
                                           ),
                                         ),
                                         Padding(
@@ -283,6 +207,7 @@ class _MyProfileState extends State<MyProfile> {
                                     ),
                                   ),
                       ),
+                      SizedBox(height: 10),
                       Expanded(
                         child: ListView(
                           children: [
@@ -304,7 +229,7 @@ class _MyProfileState extends State<MyProfile> {
                                   fillColor: myColor.myWhite,
                                   filled: true,
                                   icon: Icon(Icons.person,
-                                      color: myColor.myBlack),
+                                      color: myColor.myWhite),
                                 ),
                                 controller: nameController,
                               ),
@@ -325,7 +250,7 @@ class _MyProfileState extends State<MyProfile> {
                                     filled: true,
                                     fillColor: myColor.myWhite,
                                     icon: Icon(Icons.work,
-                                        color: myColor.myBlack)),
+                                        color: myColor.myWhite)),
                                 controller: furtherController,
                               ),
                             ),
@@ -350,7 +275,7 @@ class _MyProfileState extends State<MyProfile> {
                                     fillColor: myColor.myWhite,
                                     icon: FaIcon(
                                       FontAwesomeIcons.telegram,
-                                      color: myColor.myBlack,
+                                      color:myColor.myWhite,
                                     )),
                                 controller: telegramController,
                               ),
@@ -376,13 +301,13 @@ class _MyProfileState extends State<MyProfile> {
                                     fillColor: myColor.myWhite,
                                     icon: FaIcon(
                                       FontAwesomeIcons.github,
-                                      color: myColor.myBlack,
+                                      color: myColor.myWhite,
                                     )),
                                 controller: githubController,
                               ),
                             ),
                             SizedBox(
-                              height: 50,
+                              height: 25,
                             ),
                             Align(
                               alignment: Alignment.bottomCenter,
@@ -393,11 +318,11 @@ class _MyProfileState extends State<MyProfile> {
                                     )
                                   : Container(
                                       margin:
-                                          EdgeInsets.symmetric(horizontal: 15),
+                                          EdgeInsets.symmetric(horizontal: 15,vertical: 25),
                                       height: 50,
                                       width: double.infinity,
                                       child: RaisedButton(
-                                        color: myColor.myBlack,
+                                        color: myColor.myWhite,
                                         shape: RoundedRectangleBorder(
                                             side: BorderSide(
                                                 color: Colors.black,
@@ -409,7 +334,7 @@ class _MyProfileState extends State<MyProfile> {
                                           'Update',
                                           style: GoogleFonts.delius(
                                               fontWeight: FontWeight.w600,
-                                              color: myColor.myWhite),
+                                              color: myColor.myBlack),
                                         ),
                                         onPressed: () async {
                                           if (_formKey.currentState
